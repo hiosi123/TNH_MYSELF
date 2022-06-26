9d3d734ec46a373a9114f1716b9d981440fd1c31
hiosi123 <tls1641@gmail.com>
2022-06-27 08:49:17 +0900

    develop1

--- src/commons/libraries/utils.js
+++
@@ -1,8 +0,0 @@
-export const getDate = (date) => {
-  const newdate = new Date(date)
-  const YYYY = newdate.getFullYear()
-  const MM = newdate.getMonth() + 1
-  const dd = newdate.getDate()
-  const result = `${YYYY}-${MM}-${dd}`
-  return result
-}
